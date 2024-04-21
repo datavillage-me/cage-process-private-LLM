e2667c8c4a66a89f08d31c1ec89a3a5ffebe066b
Frederic Lebeau <frederic@datavillage.me>
2024-04-21 22:23:05 +0200

    fix requirements.txt + load remote datasets

--- process.py
+++ process.py
@@ -62,7 +62,7 @@ def process_train_event(evt: dict):
     # load the training data from data folder
     # we could also have loaded the data from an external API or from a local file (uploaded in the data  collaboration)
     
-    df = duckdb.sql("SELECT Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount,Class FROM read_parquet('./data/transactions.parquet') as transactions  JOIN read_csv(['./data/label-bank1.csv','./data/label-bank2.csv']) as labels ON (labels.UETR = transactions.UETR)").df()
+    df = duckdb.sql("SELECT Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount,Class FROM read_parquet('https://github.com/datavillage-me/cage-process-fraud-detection-example/raw/main/data/transactions.parquet') as transactions  JOIN read_csv(['https://github.com/datavillage-me/cage-process-fraud-detection-example/raw/main/data/label-bank1.csv','https://github.com/datavillage-me/cage-process-fraud-detection-example/raw/main/data/label-bank2.csv']) as labels ON (labels.UETR = transactions.UETR)").df()
     
     #feature_names = df.iloc[:, 1:30].columns
     #target = df.iloc[:1, 30:].columns
