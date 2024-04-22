c151fbc3cd2c114df77bd8a1b40523f2dae3efa3
Frederic Lebeau <frederic@datavillage.me>
2024-04-22 08:26:20 +0200

    add classification report export + some more logs

--- process.py
+++ process.py
@@ -13,6 +13,7 @@ import logging
 import time
 import requests
 import os
+import json
 import pandas as pd
 from dv_utils import default_settings, Client 
 import xgboost as xgb
@@ -61,7 +62,7 @@ def process_train_event(evt: dict):
 
     # load the training data from data folder
     # we could also have loaded the data from an external API or from a local file (uploaded in the data  collaboration)
-    
+    logger.info(f"Load data from data providers")
     df = duckdb.sql("SELECT Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount,Class FROM read_parquet('https://github.com/datavillage-me/cage-process-fraud-detection-example/raw/main/data/transactions.parquet') as transactions  JOIN read_csv(['https://github.com/datavillage-me/cage-process-fraud-detection-example/raw/main/data/label-bank1.csv','https://github.com/datavillage-me/cage-process-fraud-detection-example/raw/main/data/label-bank2.csv']) as labels ON (labels.UETR = transactions.UETR)").df()
     
     #feature_names = df.iloc[:, 1:30].columns
@@ -94,15 +95,24 @@ def process_train_event(evt: dict):
     X_undersampled = df_undersampled.iloc[:,1:30]
     Y_undersampled = df_undersampled.Class
     X_undersampled_train, X_undersampled_test, Y_undersampled_train, Y_undersampled_test = train_test_split(X_undersampled, Y_undersampled, test_size = 0.30)
+    logger.info(f"Create XGBClassifier model")
     xg_undersampled = xgb.XGBClassifier() 
     xg_undersampled.fit(X_undersampled_train, Y_undersampled_train)
-    
-    # save the model to the results location
-    xg_undersampled.save_model('/resources/outputs/model.json')
 
+    logger.info(f"Run model")
     cmat, pred = RunModel(xg_undersampled, X_undersampled_train, Y_undersampled_train, X_undersampled_test, Y_undersampled_test)
     accuracy =accuracy_score(Y_undersampled_test, pred)
+    classificationReportJson=classification_report(Y_undersampled_test, pred,output_dict=True)
+
+    logger.info(f"Save model")
+    # save the model to the results location
+    xg_undersampled.save_model('/resources/outputs/model.json')
     
+    logger.info(f"Save model classification report")
+    with open('/resources/outputs/classification-report.json', 'w', newline='') as file:
+       file.write(json.dumps(classificationReportJson))
+    
+    logger.info(f"Log accuracy")
     # push the training metrics to datavillage
     client = Client()
     client.push_metrics({"accuracy":accuracy})
@@ -113,5 +123,5 @@ def RunModel(model, X_train, y_train, X_test, y_test):
     matrix = confusion_matrix(y_test, pred)
     return matrix, pred
 
-#if __name__ == "__main__":
- #   process_train_event(None)
+if __name__ == "__main__":
+    process_train_event(None)
