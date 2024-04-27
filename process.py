a3fffa3ac00c9094c5ef36981e3836987de26bf3
Frederic Lebeau <frederic@datavillage.me>
2024-04-27 08:38:59 +0200

    fix

--- process.py
+++ process.py
@@ -72,7 +72,7 @@ def process_benchmark_event(evt: dict):
     start_time = time.time()
     #df = duckdb.sql("SELECT beneficiaries1.AIR_TIME FROM read_parquet('"+dataProvider1URL+"') as beneficiaries1 WHERE beneficiaries1.AIR_TIME IN (SELECT AIR_TIME from read_parquet('"+dataProvider2URL+"') UNION SELECT AIR_TIME from read_parquet('"+dataProvider3URL+"') UNION SELECT AIR_TIME from read_parquet('"+dataProvider4URL+"'))").df()
     df = duckdb.sql("SELECT FL_DATE from read_parquet('"+dataProvider2URL+"') UNION ALL SELECT FL_DATE from read_parquet('"+dataProvider3URL+"') UNION ALL SELECT FL_DATE from read_parquet('"+dataProvider4URL+"') UNION ALL SELECT FL_DATE from read_parquet('"+dataProvider1URL+"')")
-    #df = duckdb.sql("DESCRIBE TABLE '"+dataProvider2URL+"'") 
+    #df = duckdb.sql("DESCRIBE TABLE '"+dataProvider2URL+"'")  
     #print(df)
 
     execution_time=(time.time() - start_time)
