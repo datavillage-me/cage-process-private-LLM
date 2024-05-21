3b4e30975c07072f00e2796c62147cf04228f3d5
Frederic Lebeau <frederic@datavillage.me>
2024-05-21 18:47:27 +0200

    initial commit

--- process.py
+++ process.py
@@ -1,8 +1,5 @@
 """
-This code demonstrate how to train a XGBoost Logistic Regression model for credit card fraud detection
-The code use datasets from 3 parties
-- 2 banks providing the labels (class) for each transactions being fraudulent or not
-- A financial data intermediary or payment processor providing the transactions data on which Dimensionality Reduction Techniques for Data Privacy has been applied.
+This code demonstrate how to run a private LLM with private RAG in a controlled collaboration space
 
 """
 
@@ -17,7 +14,11 @@ import duckdb
 
 from dv_utils import default_settings, Client 
 
-import pandas as pd
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+from llama_index.core import Document
+from llama_index.vector_stores.duckdb import DuckDBVectorStore
+from llama_index.core import Document
 
 logger = logging.getLogger(__name__)
 
@@ -41,8 +42,8 @@ def event_processor(evt: dict):
 
     # dispatch events according to their type
     evt_type =evt.get("type", "")
-    if(evt_type == "QUERY"):
-        process_query_event(evt)
+    if(evt_type == "LOAD"):
+        process_load_event(evt)
     else:
         generic_event_processor(evt)
 
@@ -51,101 +52,73 @@ def generic_event_processor(evt: dict):
     # push an audit log to reccord for an event that is not understood
     logger.info(f"Received an unhandled event {evt}")
 
-def process_query_event(evt: dict):
+def process_load_event(evt: dict):
     """
-    Train an XGBoost Classifier model using the logic given in 
+    Load RAG into private LLM
      """
 
     logger.info(f"--------------------------------------------------")
-    logger.info(f"|               START BENCHMARKING               |")
+    logger.info(f"|               START CONNNECTING RAG            |")
     logger.info(f"|                                                |")
-    # load the training data from data providers
-    # duckDB is used to load the data and aggregated them in one single datasets
+    logger.info(f"| 1. Load pretrained SentenceTransformer         |")
+    # loads BAAI/bge-small-en-v1.5, embed dimension: 384, max token limit: 512
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
     logger.info(f"| 1. Load data from data providers               |")
+    from llama_index.core import (
+        StorageContext,
+        ServiceContext,
+        VectorStoreIndex,
+        SimpleDirectoryReader,
+    )
+
+    # DuckDB integration for storing and retrieving from knowledge base
+    
+
+
     logger.info(f"|    https://github.com/./demographic.parquet |")
     logger.info(f"|    https://github.com/./patients.parquet |")
-    dataProvider1URL="https://github.com/datavillage-me/cage-process-clinical-trial-patient-cohort-selection/raw/main/data/demographic.parquet"
-    #dataProvider1URL="data/demographic.parquet"
-    dataProvider2URL="https://github.com/datavillage-me/cage-process-clinical-trial-patient-cohort-selection/raw/main/data/patients.parquet"
-    #dataProvider2URL="data/patients.parquet"
+    dataProvider1URL="https://docs.google.com/document/u/0/export?format=txt&id=18SSEVxTuCmSVgM-fMcshRnNHtFVhBNMNDchxA4KqY-Q&token=AC4w5Vib0viWL7OJkKzubeJ1veTiKvq_Pg%3A1715955647070&ouid=117841664004706563551&includes_info_params=true&usp=drive_web&cros_files=false"
+    dataProvider2URL="https://docs.google.com/document/u/0/export?format=txt&id=1uNB1SFT8mZ33sVGG-mFGHCSuOj3fu0ZktRDZHYyU2S8&token=AC4w5Vib0viWL7OJkKzubeJ1veTiKvq_Pg%3A1715955647070&ouid=117841664004706563551&includes_info_params=true&usp=drive_web&cros_files=false"
     start_time = time.time()
     logger.info(f"|    Start time:  {start_time} secs |")
+    df = duckdb.sql("SELECT content FROM read_text(['"+dataProvider1URL+"','"+dataProvider2URL+"'])").df()
     
-    whereClause=evt.get("parameters", "")
-    if whereClause!='':
-        baseQuery="SELECT COUNT(*) as total from '"+dataProvider1URL+"' as demographic,'"+dataProvider2URL+"' as patients WHERE demographic.national_id=patients.national_id AND "+whereClause
-    else:
-        baseQuery="SELECT COUNT(*) as total from '"+dataProvider1URL+"' as demographic,'"+dataProvider2URL+"' as patients WHERE demographic.national_id=patients.national_id"
-    
-    #total candidates
-    df = duckdb.sql(baseQuery).df()
-    totalCandidates=df['total'][0]
-    print(totalCandidates)
-    
-    #gender
-    #male
-    df = duckdb.sql(baseQuery+ " AND demographic.gender='male'").df()
-    totalGenderMale=df['total'][0]
-    #female
-    df = duckdb.sql(baseQuery+ " AND demographic.gender='female'").df()
-    totalGenderFemale=df['total'][0]
 
-    #education_level
-    #high_school
-    df = duckdb.sql(baseQuery+ " AND demographic.education_level='high_school'").df()
-    totalEducationLevelHighSchool=df['total'][0]
-    #college
-    df = duckdb.sql(baseQuery+ " AND demographic.education_level='college'").df()
-    totalEducationLevelCollege=df['total'][0]
-    #university
-    df = duckdb.sql(baseQuery+ " AND demographic.education_level='university'").df()
-    totalEducationLevelUniversity=df['total'][0]
+    documents = [Document(text=df['content'][ind]) for ind in df.index]
+   
+    # Set the size of the chunk to be 512 tokens
+    llm = Ollama(model="llama3", base_url="http://127.0.0.1:11434",request_timeout=600.0)
+    documents_service_context = ServiceContext.from_defaults(llm=llm,
+    embed_model=embed_model,
+    chunk_size=512)
+    vector_store = DuckDBVectorStore(
+        embed_dim=384,
+    )
 
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-    #employment_status
-    #unemployed
-    df = duckdb.sql(baseQuery+ " AND demographic.employment_status='unemployed'").df()
-    totalEmploymentStatusUnemployed=df['total'][0]
-    #employed
-    df = duckdb.sql(baseQuery+ " AND demographic.employment_status='employed'").df()
-    totalEmploymentStatusEmployed=df['total'][0]
-    #student
-    df = duckdb.sql(baseQuery+ " AND demographic.employment_status='student'").df()
-    totalEmploymentStatusStudent=df['total'][0]
-    #retired
-    df = duckdb.sql(baseQuery+ " AND demographic.employment_status='retired'").df()
-    totalEmploymentStatusRetired=df['total'][0]
+    knowledge_base = VectorStoreIndex.from_documents(
+        documents,
+        storage_context=storage_context,
+        embed_model=embed_model,
+        service_context=documents_service_context,
+    )
 
-    execution_time=(time.time() - start_time)
-    logger.info(f"|    Execution time:  {execution_time} secs |")
+    # The query engine
+    query_engine = knowledge_base.as_query_engine(llm=llm)
 
-    logger.info(f"| 2. Save outputs of the collaboration           |")
-    # The output file model is stored in the data folder
-    
-    output= ''' {
-    "candidates": '''+str(totalCandidates)+''',
-        "gender": {
-        "male":'''+str(totalGenderMale)+''',
-        "female":'''+str(totalGenderFemale)+'''
-        },
-        "education_level": {
-        "high_school":'''+str(totalEducationLevelHighSchool)+''',
-        "college":'''+str(totalEducationLevelCollege)+''',
-        "university":'''+str(totalEducationLevelUniversity)+'''
-        },
-        "employment_status":{
-        "unemployed":'''+str(totalEmploymentStatusUnemployed)+''',
-        "employed":'''+str(totalEmploymentStatusEmployed)+''',
-        "student":'''+str(totalEmploymentStatusStudent)+''',
-        "retired":'''+str(totalEmploymentStatusRetired)+'''
-        }
-    } '''
+    # Run a query
+    #answer = query_engine.query("Test, answer 1 word")
+    #print (answer)
 
-    #with open('data/my.json', 'w', newline='') as file:
-        #file.write(output)
+    # # Run a query
+    # answer = query_engine.query("What are the main actions out of the board of february and April?")
+    # print (answer)
 
-    with open('/resources/outputs/candidates-report.json', 'w', newline='') as file:
-        file.write(output)
+    #  # Run a query
+    # answer = query_engine.query("Could you give me the highlights of both board, February and April?")
+    # print (answer)
    
     logger.info(f"|                                                |")
     logger.info(f"--------------------------------------------------")
@@ -153,7 +126,6 @@ def process_query_event(evt: dict):
 
 if __name__ == "__main__":
     test_event = {
-            "type": "QUERY",
-            "parameters": ""
+            "type": "LOAD"
     }
-    process_query_event(test_event)
+    process_load_event(test_event)
